2a7ca7277ed14fe177546c98a320d426f880f1f3
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-24 14:18:24 +0100

    retrieve user data now gets all the user data

--- lib/data_extractor.rb
+++ lib/data_extractor.rb
@@ -2,9 +2,16 @@ require 'httparty'
 
 class DataExtractor
   def retrieve_user_data
-    url = 'https://driftrock-dev-test-2.herokuapp.com/users?page=1&per_page=20'
-    response = HTTParty.get(url)
-    response.parsed_response
+    user_data = []
+    page_number = 1
+    loop do
+      url = "https://driftrock-dev-test-2.herokuapp.com/users?page=#{page_number}&per_page=100"
+      response = HTTParty.get(url)
+      break if response.parsed_response['data'] == []
+      user_data += response.parsed_response['data']
+      page_number += 1
+    end
+    user_data
   end
 
   def retrieve_purchase_data
