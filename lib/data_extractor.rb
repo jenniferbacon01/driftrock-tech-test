bf42135d51072cc33bb2a188efa09a6a57e2f53f
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-24 14:51:24 +0100

    refactored duplicate api request methods

--- lib/data_extractor.rb
+++ lib/data_extractor.rb
@@ -1,22 +1,26 @@
 require 'httparty'
 
 class DataExtractor
-  def retrieve_user_data
-    user_data = []
-    page_number = 1
-    loop do
-      url = "https://driftrock-dev-test-2.herokuapp.com/users?page=#{page_number}&per_page=100"
-      response = HTTParty.get(url)
-      break if response.parsed_response['data'] == []
-      user_data += response.parsed_response['data']
-      page_number += 1
-    end
-    user_data
+  def retrieve_users_data
+    retrieve_data('users')
   end
 
-  def retrieve_purchase_data
-    url = 'https://driftrock-dev-test-2.herokuapp.com/purchases?page=1&per_page=20'
-    response = HTTParty.get(url)
-    response.parsed_response
+  def retrieve_purchases_data
+    retrieve_data('purchases')
+  end
+
+  private
+
+  def retrieve_data(data_category)
+    data = []
+    page_number = 1
+    loop do
+      url = "https://driftrock-dev-test-2.herokuapp.com/#{data_category}?page=#{page_number}&per_page=1000"
+      response = HTTParty.get(url)
+      break if response.parsed_response['data'] == []
+      data += response.parsed_response['data']
+      page_number += 1
+    end
+    data
   end
 end
--- spec/data_extractor_spec.rb
+++ spec/data_extractor_spec.rb
@@ -4,10 +4,10 @@ describe DataExtractor do
   subject(:data_extractor) { described_class.new }
 
   it 'gets user data from the API' do
-    expect(data_extractor.retrieve_user_data).not_to eq nil
+    expect(data_extractor.retrieve_users_data).not_to eq nil
   end
 
   it 'gets purchase data from the API' do
-    expect(data_extractor.retrieve_purchase_data).not_to eq nil
+    expect(data_extractor.retrieve_purchases_data).not_to eq nil
   end
 end
